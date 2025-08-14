fa6755e06372779c3583ce3cc03434bf05d8e0b3
Vladislav Kosevich <v.kosevich@aiby.com>
2025-08-14 19:03:24 +0300

    feat: messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { useTRPC } from '@/trpc/client'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
 import { toast } from 'sonner'
 
@@ -11,10 +11,11 @@ const Page = () => {
 	const [value, setValue] = useState('')
 
 	const trpc = useTRPC()
-	const invoke = useMutation(
-		trpc.invoke.mutationOptions({
+	const { data: messages } = useQuery(trpc.messages.getMany.queryOptions())
+	const createMessage = useMutation(
+		trpc.messages.create.mutationOptions({
 			onSuccess: () => {
-				toast.success('Backgtound job started')
+				toast.success('Message created')
 			},
 		})
 	)
@@ -23,11 +24,12 @@ const Page = () => {
 		<div className='p-4 max-w-7xl mx-auto'>
 			<Input value={value} onChange={e => setValue(e.target.value)} />
 			<Button
-				disabled={invoke.isPending}
-				onClick={() => invoke.mutate({ value: value })}
+				disabled={createMessage.isPending}
+				onClick={() => createMessage.mutate({ value: value })}
 			>
 				Invoke background job
 			</Button>
+			{JSON.stringify(messages, null, 2)}
 		</div>
 	)
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,23 +4,30 @@ import {
 	createNetwork,
 	createTool,
 	openai,
+	type Tool,
 } from '@inngest/agent-kit'
 
 import z from 'zod'
 import { PROMPT } from '@/prompt'
 import { inngest } from './client'
 import { getSandbox, lastAssistantTextMessageContent } from './utils'
+import { prisma } from '@/lib/db'
 
-export const helloWorld = inngest.createFunction(
-	{ id: 'hello-world' },
-	{ event: 'test/hello.world' },
+interface AgentState {
+	summary: string
+	files: { [path: string]: string }
+}
+
+export const codeAgentFunction = inngest.createFunction(
+	{ id: 'code-agent' },
+	{ event: 'code-agent/run' },
 	async ({ event, step }) => {
 		const sandboxId = await step.run('get-sandbox-id', async () => {
 			const sandbox = await Sandbox.create('luci-ai-nextjs')
 			return sandbox.sandboxId
 		})
 
-		const codeAgent = createAgent({
+		const codeAgent = createAgent<AgentState>({
 			name: 'code-agent',
 			system: PROMPT,
 			model: openai({
@@ -70,7 +77,10 @@ export const helloWorld = inngest.createFunction(
 							})
 						),
 					}),
-					handler: async ({ files }, { step, network }) => {
+					handler: async (
+						{ files },
+						{ step, network }: Tool.Options<AgentState>
+					) => {
 						const newFiles = await step?.run(
 							'createOrUpdateFiles',
 							async () => {
@@ -131,7 +141,7 @@ export const helloWorld = inngest.createFunction(
 			},
 		})
 
-		const network = await createNetwork({
+		const network = await createNetwork<AgentState>({
 			name: 'coding-agent-network',
 			agents: [codeAgent],
 			maxIter: 15,
@@ -146,10 +156,41 @@ export const helloWorld = inngest.createFunction(
 
 		const result = await network.run(event.data.value)
 
+		const isError =
+			!result.state.data.summary ||
+			Object.keys(result.state.data.files || {}).length === 0
+
 		const sandboxUrl = await step.run('get-sandbox-url', async () => {
 			const sandbox = await getSandbox(sandboxId)
 			const host = await sandbox.getHost(3000)
 			return `https://${host}`
+		})
+
+		await step.run('save-result', async () => {
+			if (isError) {
+				return await prisma.message.create({
+					data: {
+						content: 'Something went wrong. Please try again',
+						role: 'ASSISTANT',
+						type: 'ERROR',
+					},
+				})
+			}
+
+			return await prisma.message.create({
+				data: {
+					content: result.state.data.summary,
+					role: 'ASSISTANT',
+					type: 'RESULT',
+					fragment: {
+						create: {
+							sandboxUrl: sandboxUrl,
+							title: 'Fragment',
+							files: result.state.data.files,
+						},
+					},
+				},
+			})
 		})
 
 		return {
