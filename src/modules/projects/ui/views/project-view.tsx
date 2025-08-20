c25d1f368506ad85393b53b2c2d96c0ab57b738f
Vladislav Kosevich <v.kosevich@aiby.com>
2025-08-20 18:18:53 +0300

    feat: fragment view

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -11,6 +11,7 @@ import {
 
 import { MessagesContainer } from '../components/messages-container'
 import { ProjectHeader } from '../components/project-header'
+import { FragmentWeb } from '../components/fragment-web'
 
 interface Props {
 	projectId: string
@@ -40,7 +41,7 @@ export const ProjectView = ({ projectId }: Props) => {
 				</ResizablePanel>
 				<ResizableHandle withHandle />
 				<ResizablePanel defaultSize={65} minSize={50}>
-					TODO: Preview
+					{!!activeFragment && <FragmentWeb data={activeFragment} />}
 				</ResizablePanel>
 			</ResizablePanelGroup>
 		</div>
